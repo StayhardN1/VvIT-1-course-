aef9d0863807928023525baaf4c69e9f0bd4f854
StayhardN1 <kurban0607@icloud.com@whatever.com>
2024-12-18 13:15:31 +0300

    Лаба 4

---
+++ lab4/main.py
@@ -0,0 +1,8 @@
+import math
+number = 16
+square_root = math.sqrt(number)
+print(f"Квадратный корень числа {number} равен {square_root}")
+
+from datetime import datetime
+current_datetime = datetime.now()
+print(f"Текущая дата и время: {current_datetime}")
---
+++ lab4/maina.py
@@ -0,0 +1,5 @@
+import mymodule
+result = mymodule.sum_ch(6, 5)
+result2 = mymodule.fib(6)
+result3 = mymodule.max_ch(6, 5)
+print(result, result2, result3)
